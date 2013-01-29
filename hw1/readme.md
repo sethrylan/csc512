ee6de5e0752a18ef8eb6c998a331e2d0fe4e45f1
sethrylan <seth.rylan@gmail.com>
2013-01-29 12:44:51 -0500

    added automated tests

--- hw1/readme.md
+++ hw1/readme.md
@@ -16,73 +16,11 @@ Three executables produced:
 calc and calc2 can be used in interpretive-mode and exitted with ^C; pseudo expects EOF to end.
 
 	
-# Valid Test Input
-```
-VAR a,b:REAL;c:INT[42];
-BEGIN
-c:=thx[1138]*INT(10);
-a:=-1.0+0.20/0.1;
-IF b
-	>
-1 AND b<>b OR NOT b=b THEN WRITE(a); ELSE READ(a); ENDIF;
-a:=110/10;
-END
-```
-```
-VAR
-BEGIN
-END
-```
-```
-BEGIN
-END
-```
-```
-BEGIN
-PROC procName() BEGIN END ENDPROC;
-PROC betterProc(IN in:INT, OUT out:INT, INOUT inout:INT, REF r:REAL) BEGIN END ENDPROC;
-procName(no, arguments, here);
-betterProc(really, arguments);
-END
-```
-```
-BEGIN
-a := ++++10;
-a := --10;
-a := -+-+-10;
-a := +-+-+--10;
-END
-```
-```
-BEGIN
-WHILE a=b DO
-	REPEAT
-		a:=c;
-		UNTIL a<>b
-	ENDREPEAT;
-	FOR i:=5 MOD 1 TO 10 DIV 2 DO ENDFOR;
-	PARFOR ii:=iii TO 5*5+2 PRIVATE x,y,z REDUCE - xx,yy,zz DO 
-		WRITE(ii);
-	ENDPARFOR;
-ENDWHILE;
-END
-```
+### Automated Testing
 
-### Invalid Test Input 
-```
-a:=-1.0+2.0;
-IF a THEN WRITE(a) ENDIF;
-IF a>0 THEN WRITE(a) ENDIF;
-a:=1;
-a-;
-test scanner #;
-```
-```
-VAR a,b:REAL;
-BEGIN
-a:=-1.0+0.20;
-IF b>1 THEN WRITE(PRIVATE); ENDIF;
-a:=110;
-END
-```
+See ```test/``` folder for input and expected outputs. Run ```make test``` to run tests.
+
+
+
+
 
--- hw1/scan.c
+++ hw1/scan.c
@@ -15,11 +15,7 @@ FILE *yyin;
 char* yytext;
 int yylineno;
 
-/*
-int yywrap(void){
-        return 1;
-}
-*/
+//%option noyywrap
 
 int is_op(int c) {
 	if(c == '-' || c == '+' || c == '*' || c == '/' || c == '\n') {
@@ -29,11 +25,6 @@ int is_op(int c) {
 	}
 }
 
-//char append(char *out, char *in) {
-//    strcat(out, in);
-//    return out;
-//}
-
 int yylex(void) {
 	if(!yyin) {
 		yyin = stdin;
@@ -42,99 +33,68 @@ int yylex(void) {
 	int c;
 
 	while(c = getc(yyin)){
-	char buffer[] = "";
+		char buffer[] = "";
 
-	s0:
-	if(isdigit(c)) {
-		sprintf(buffer, "%s%c", buffer, c);
-		c = getc(yyin);
-		goto s3;
-	} else if(c=='.') {
-		sprintf(buffer, "%s%c", buffer, c);
-		c = getc(yyin);
-		goto s2;
-	} else if(is_op(c)) {
-		goto s5;
-	} else {
-		goto err;
-	}
-
-	s2:
-	if(isdigit(c)) {
-		sprintf(buffer, "%s%c", buffer, c);
-		c = getc(yyin);
-		goto s4;
-	} else {
-		yylval.double_val = atof(buffer);
-		ungetc(c, yyin);
-		return NUMBER;
-	}
-
-	s3:
-	if (isdigit(c)) {
-		sprintf(buffer, "%s%c", buffer, c);
-		c = getc(yyin);
-		goto s3;
-	} else if(c=='.') {
-		sprintf(buffer, "%s%c", buffer, c);
-		c = getc(yyin);
-		goto s2;
-	} else {
-		yylval.double_val = atof(buffer);
-		ungetc(c, yyin);
-		return NUMBER;
-	}
-
-	s4:
-	if (isdigit(c)) {
-		sprintf(buffer, "%s%c", buffer, c);
-		c = getc(yyin);
-		goto s4;
-	} else {
-		yylval.double_val = atof(buffer);
-		ungetc(c, yyin);
-		return NUMBER;
-	}
-
-	s5:
-	if(c == '\n') {
-		yylineno++;
-	}
-	return c;
-	
-	err:
-	yyerror("Unknown character");
-
-}
-/*
-	while(c=getc(yyin)) {
+		s0:
 		if(isdigit(c)) {
-			double value = c - '0';
-			while(isdigit(c = getc(yyin))) {
-				value = (10 * value) + (c - '0');
-			}
-			yylval.double_val = value;
+			sprintf(buffer, "%s%c", buffer, c);
+			c = getc(yyin);
+			goto s3;
+		} else if(c=='.') {
+			sprintf(buffer, "%s%c", buffer, c);
+			c = getc(yyin);
+			goto s2;
+		} else if(is_op(c)) {
+			goto s5;
+		} else {
+			goto err;
+		}
+
+		s2:
+		if(isdigit(c)) {
+			sprintf(buffer, "%s%c", buffer, c);
+			c = getc(yyin);
+			goto s4;
+		} else {
+			yylval.double_val = atof(buffer);
 			ungetc(c, yyin);
 			return NUMBER;
 		}
-		if(is_op(c)) {
-			if(c == '\n') {
-				yylineno++;
-			}
-			return c;
+
+		s3:
+		if (isdigit(c)) {
+			sprintf(buffer, "%s%c", buffer, c);
+			c = getc(yyin);
+			goto s3;
+		} else if(c=='.') {
+			sprintf(buffer, "%s%c", buffer, c);
+			c = getc(yyin);
+			goto s2;
+		} else {
+			yylval.double_val = atof(buffer);
+			ungetc(c, yyin);
+			return NUMBER;
 		}
-		if(c != ' ' || c != '\t') {
-		       yyerror("Unknown character");
+
+		s4:
+		if (isdigit(c)) {
+			sprintf(buffer, "%s%c", buffer, c);
+			c = getc(yyin);
+			goto s4;
+		} else {
+			yylval.double_val = atof(buffer);
+			ungetc(c, yyin);
+			return NUMBER;
 		}
+
+		s5:
+		if(c == '\n') {
+			yylineno++;
+		}
+		return c;
+
+		err:
+		yyerror("Unknown character");
+
 	}
-
-*/
-
-// scan number
-//	if(isdigit(c) || c == '.') {
-//		ungetc(c, yyin);
-//		scanf("%lf", &(yylval.double_val));
-//		return NUMBER;
-//	}
-
 }
