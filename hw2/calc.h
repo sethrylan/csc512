a32870d01f0b4bc60f748f545e33b193ac61db0b
sethrylan <seth.rylan@gmail.com>
2013-02-09 13:01:36 -0500

    updated

--- hw2/calc.h
+++ hw2/calc.h
@@ -1,39 +1,13 @@
-#ifndef YYTOKENTYPE
-# define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     NUMBER = 258,
-     PLUS   = 259,
-     MINUS  = 260,
-     DIV    = 261,
-     MULT   = 262,
-     LPAREN = 263,
-     RPAREN = 264,
-     EOL    = 265
-   };
-#endif
+int yylex(void);
+extern char* yytext;
+extern int yylineno;
 
+typedef enum {NUMBER, PLUS, MINUS, DIV, MULT, LPAREN, RPAREN, EOL} Symbol;
 
-
-#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
 {
-
-/* Line 2068 of yacc.c  */
-#line 9 "calc.y"
-
         double double_val;
-
-
-
-/* Line 2068 of yacc.c  */
-#line 59 "calc.tab.h"
 } YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
-#endif
 
+//extern YYSTYPE yylval;
 extern YYSTYPE yylval;
-
